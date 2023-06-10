0b8f93d827445b17aeeb47d4c1ae12ad19f62988
Michael Cantú <12145912+mcantu@users.noreply.github.com>
2023-06-09 23:52:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// Importing required modules
 const core = require('@actions/core')
   , enterprise = require('./src/enterprise')
   , githubClient = require('./src/github')
   ;
 
+// Function to get the required input value
 function getRequiredInputValue(key) {
   return core.getInput(key, { required: true });
 }
 
+// Main function
 async function run() {
   try {
+    // Getting the required input values
     const githubToken = getRequiredInputValue('github_token')
       , metadataSection = core.getInput('metadata_section')
       , customCidrs = core.getInput('custom_cidrs')
@@ -16,71 +20,58 @@ async function run() {
       , isActive = core.getInput('active') === 'true'
       ;
 
+    // Creating an instance of the GitHub client
     const octokit = githubClient.create(githubToken);
+
+    // Getting the target enterprise
     const targetEnterprise = await enterprise.getEnterprise(enterpriseSlug, octokit);
     core.info(`Enterprise account: ${targetEnterprise.name} : ${targetEnterprise.url}`);
 
+    // Validating input values
     if (!metadataSection && !customCidrs) {
       throw new Error('A set of custom CIDRS or GitHub meta CIDRs section must be specified.');
     }
 
+    // Adding GitHub meta CIDRs to the enterprise
     if (metadataSection) {
       const cidrs = await getMetaCIDRs(octokit, metadataSection);
       if (cidrs) {
         core.info(`GitHub meta CIDRs to add: ${JSON.stringify(cidrs)}`);
         await addCidrsToEnterprise(targetEnterprise, cidrs, isActive, `GitHub Meta CIDR for ${metadataSection}`);
-        // Retrieve the IP allow list entries for the metadata section and log their isActive property
-        const metadataIpAllowListEntries = await targetEnterprise.getEnterpriseIpAllowListEntries({ metadata: metadataSection });
-        for (const entry of metadataIpAllowListEntries) {
-          console.log(`IP allow list entry ${entry.name} for metadata section ${metadataSection} is active: ${entry.isActive}`);
-        }
       } else {
         throw new Error(`The metadata CIDRs for '${metadataSection}' were unable to be resolved.`);
       }
     }
 
+    // Adding custom CIDRs to the enterprise
     if (customCidrs) {
       const cidrs = getCidrs(customCidrs);
       core.info(`Custom CIDRs to add: ${JSON.stringify(cidrs)}`);
       await addCidrsToEnterprise(targetEnterprise, cidrs, isActive, core.getInput('custom_cidrs_label'));
-  
-      const ipAllowListEntries = await targetEnterprise.getEnterpriseIpAllowListEntries();
-      // Loop through the IP allow list entries and log their isActive property
-      for (const entry of ipAllowListEntries) {
-        console.log(`IP allow list entry ${entry.name} is active: ${entry.isActive}`);
-      }
     }
   } catch (err) {
     core.setFailed(err);
   }
 }
 
+// Calling the main function
 run();
 
-async function addCidrsToEnterprise(enterprise, cidrs, label) {
-  const ipAllowListEntries = await enterprise.getEnterpriseIpAllowListEntries();
-  for (const entry of ipAllowListEntries) {
-    if (entry.name === label) {
-      core.startGroup(`Updating IP Allow List Entry: ${label}`);
-      await entry.update({ cidrs, isActive: true });
-      core.endGroup();
-      return;
+// Function to add CIDRs to the enterprise
+async function addCidrsToEnterprise(targetEnterprise, cidrs, isActive, name) {
+  for (const cidr of cidrs) {
+    const existingEntry = await getIpAllowListEntry(targetEnterprise, cidr);
+    if (existingEntry && !existingEntry.isActive) {
+      await updateIpAllowListEntry(targetEnterprise, existingEntry.id, isActive);
+      core.info(`Enabled existing IP allow list entry for ${cidr}`);
+    } else if (!existingEntry) {
+      const newEntry = await targetEnterprise.addAllowListCIDRs(name, [cidr], isActive);
+      core.info(`Added new IP allow list entry for ${cidr}`);
     }
   }
-
-  core.startGroup(`Building IP Allow List Entry: ${label}`);
-  await enterprise.addAllowListCIDRs(label, cidrs, true);
-  core.endGroup();
 }
 
-async function updateAllIpAllowListEntries(enterprise) {
-  const ipAllowListEntries = await enterprise.getEnterpriseIpAllowListEntries();
-  for (const entry of ipAllowListEntries) {
-    core.info(`Updating IP Allow List Entry: ${entry.name}`);
-    await addCidrsToEnterprise(enterprise, entry.cidrs, entry.name);
-  }
-}
-
+// Function to get the GitHub meta CIDRs
 async function getMetaCIDRs(octokit, name) {
   const results = await octokit.rest.meta.get();
   core.info(`Loaded GitHub Meta API CIDRs`);
@@ -88,6 +79,7 @@ async function getMetaCIDRs(octokit, name) {
   return results.data[name];
 }
 
+// Function to get the CIDRs from the input value
 function getCidrs(value) {
   const cidrs = value.split(',');
 
